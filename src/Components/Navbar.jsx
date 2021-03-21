75e5d41584a037f5f75c891d99a68903ed385a9b
Krzysiek Makówka <krzysiekmakowka9@gmail.com>
2021-03-21 18:43:45 +0100

    added two slides into navbar comp

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -5,21 +5,40 @@ import { scroller } from "react-scroll";
 import "../Styles/Navbar.css";
 
 const Navbar = () => {
+  const scrollToHeader = () => {
+    scroller.scrollTo("header-section", {
+      duration: 1500,
+      delay: 100,
+      smooth: "easeOutQuad",
+    });
+  };
   const scrollToAboutMe = () => {
     scroller.scrollTo("waves", {
       duration: 1500,
       delay: 100,
-      smooth: " easeOutQuad",
+      smooth: "easeOutQuad",
       offset: 110,
+    });
+  };
+  const scrollToSkills = () => {
+    scroller.scrollTo("skills-section", {
+      duration: 1500,
+      delay: 100,
+      smooth: "easeOutQuad",
+      offset: -40,
     });
   };
   return (
     <nav className="navbar navbar-expand-lg navbar-dark fixed-top ">
       <div className="container">
         <div className="gradient"> </div>
-        <li className="navbar-brand" href="#">
+        <a
+          className="navbar-brand"
+          href="#header-section "
+          onClick={() => scrollToHeader()}
+        >
           Welcome in my world of programming
-        </li>
+        </a>
         <button
           className="navbar-toggler"
           type="button"
@@ -35,17 +54,22 @@ const Navbar = () => {
           <ul className="navbar-nav me-auto mb-2 mb-lg-0 ml-auto">
             <li className="nav-item">
               <a
-                onClick={() => scrollToAboutMe()}
-                className="nav-link "
                 aria-current="page"
-                href="#"
+                href="#about-me"
+                onClick={() => scrollToAboutMe()}
+                className="nav-link"
               >
                 About Me
               </a>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">
-                My learning procees
+              <a
+                aria-current="page"
+                className="nav-link"
+                href="#skills"
+                onClick={() => scrollToSkills()}
+              >
+                Skills
               </a>
             </li>
             <li className="nav-item">
